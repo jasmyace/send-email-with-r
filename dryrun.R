62ce53d9c5a1ca8984d8e4572e45fa8b0f1a4d88
Jason <jmitchell@west-inc.com>
2017-03-07 08:23:29 -0700

    Change dryrun to reflect my email.  (Outside of git, perform gmail authentication.)

--- dryrun.R
+++ dryrun.R
@@ -1,12 +1,12 @@
 library(gmailr)
 
 ## edit line below to reflect YOUR json credential filename
-use_secret_file("gmailr-tutorial.json")
+#use_secret_file("gmailr-tutorial.json")
 
 ## edit below with email addresses from your life
 test_email <- mime(
-  To = "PUT_A_VALID_EMAIL_ADDRESS_THAT_YOU_CAN_CHECK_HERE",
-  From = "PUT_THE_GMAIL_ADDRESS_ASSOCIATED_WITH_YOUR_GOOGLE_ACCOUNT_HERE",
+  To = "jmitchell@west-inc.com",
+  From = "jmitchell@west-inc.com",
   Subject = "this is just a gmailr test",
   body = "Can you hear me now?")
 
