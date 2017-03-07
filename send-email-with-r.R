dcaa17d7f7000d1a8678714ba412cee5cb819d94
Jason <jmitchell@west-inc.com>
2017-03-07 09:04:02 -0700

    In theory, the final commit for this repository.  The original function has been updated and included in the EnvCovDB package.

--- send-email-with-r.R
+++ send-email-with-r.R
@@ -3,46 +3,50 @@ suppressPackageStartupMessages(library(dplyr))
 suppressPackageStartupMessages(library(purrr))
 library(readr)
 
-addresses <- read_csv("addresses.csv")
-marks <- read_csv("marks.csv")
-my_dat <- left_join(marks, addresses)
+#setwd("//lar-file-srv/Data/PSMFC_CampRST/send-email-with-r")
 
-this_hw <- "The Fellowship Of The Ring"
-email_sender <- 'Peter Jackson <peter@tolkien.example.org>' # your Gmail address
-optional_bcc <- 'Anonymous <anon@palantir.example.org>'     # for me, TA address
-body <- "Hi, %s.
+addrStem <- "//lar-file-srv/Data/PSMFC_CampRST/send-email-with-r"
+my_dat <- read.csv(paste0(addrStem,"/addresses.csv"))
 
-Your mark for %s is %s.
+#   ---- Perhaps reduce the full list here, say, for testing.
+my_dat <- my_dat[my_dat$firstName %in% c("Jason"),]
 
-Thanks for participating in this film!
+this_update  <- paste0("Environmental Convariate Database -- Update for ",Sys.Date())
+email_sender <- 'Environmental Covariate Database DBA <jmitchell@west-inc.com>' # your Gmail address
+body         <- "Hello, %s.
+
+The update completed!  
+
+Maybe more later...
+
+Thanks,
+The Environmental Covariate Database
 "
 
 edat <- my_dat %>%
   mutate(
-    To = sprintf('%s <%s>', name, email),
-    Bcc = optional_bcc,
+    To = sprintf('%s <%s>', firstName, email),
     From = email_sender,
-    Subject = sprintf('Mark for %s', this_hw),
-    body = sprintf(body, name, this_hw, mark)) %>%
-  select(To, Bcc, From, Subject, body)
+    Subject = this_update,
+    body = sprintf(body, firstName)) %>%
+  select(To, From, Subject, body)
 edat
-write_csv(edat, "composed-emails.csv")
+write_csv(edat, paste0("composed-emails",Sys.Date(),".csv"))
 
 emails <- edat %>%
-  map_n(mime)
+  pmap(mime)
 
 ## optional: use if you've created your own client id
-use_secret_file("gmailr-tutorial.json")
-
+# use_secret_file("gmailr-tutorial.json")
+# 
 safe_send_message <- safely(send_message)
 sent_mail <- emails %>%
   map(safe_send_message)
 
-saveRDS(sent_mail,
-        paste(gsub("\\s+", "_", this_hw), "sent-emails.rds", sep = "_"))
-
-errors <- sent_mail %>%
-  transpose() %>%
-  .$error %>%
-  map_lgl(Negate(is.null))
-sent_mail[errors]
+saveRDS(sent_mail,paste(gsub("\\s+", "_", this_update), "sent-emails.rds", sep = "_"))
+# 
+# errors <- sent_mail %>%
+#   transpose() %>%
+#   .$error %>%
+#   map_lgl(Negate(is.null))
+# sent_mail[errors]
